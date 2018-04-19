7031381135d7f5e92f9c57531714afc9a0de64e2
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-04-19 14:46:26 +0900

    update

--- UIPickView03/ViewController.swift
+++ UIPickView03/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate {
+    
 
+    
+    @IBOutlet weak var myPickrView: UIPickerView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var mylbl02: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myPickrView.delegate = self
+        myPickrView.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 10
+        } else {
+            return 100
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return "First\(row)"
+        } else {
+            return "second \(row)"
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            myLabel.text = "First \(row)"
+        }else{
+            mylbl02.text = "second\(row)"
+        }
+        
+    }
+    
 }
 
